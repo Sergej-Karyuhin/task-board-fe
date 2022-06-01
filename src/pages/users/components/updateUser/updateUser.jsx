aaee17c1a088c84ef1ca12ac90ed32bf467e1eb4
Siarhei Karukhin <siarhei_karukhin@epam.com>
2022-06-01 12:15:28 +0300

    feat: add update for user

---
+++ src/pages/users/components/updateUser/updateUser.jsx
@@ -0,0 +1,52 @@
+import { updateUser } from '../../../../services/users';
+
+import styles from './updateUser.module.scss';
+
+const UpdateUser = ({ users, setUsers, userForUpdate, setUserForUpdate }) => {
+  const onSubmit = (event) => {
+    const qqq = (user) => {
+      const q = users.filter((item) => item.id !== user.id);
+      const w = { id: user.id, name: user.name, login: user.login };
+      setUsers([...q, w]);
+      setUserForUpdate(w);
+    };
+    updateUser(userForUpdate).then(qqq);
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <p>Update user:</p>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <label className={styles.label}>
+          Name:
+          <input
+            type="text"
+            value={userForUpdate?.name ?? ''}
+            onChange={(event) => setUserForUpdate({ ...userForUpdate, ...{ name: event.target.value } })}
+          />
+        </label>
+        <label className={styles.label}>
+          Login:
+          <input
+            type="text"
+            value={userForUpdate?.login ?? ''}
+            onChange={(event) => setUserForUpdate({ ...userForUpdate, ...{ login: event.target.value } })}
+          />
+        </label>
+        <label className={styles.label}>
+          Password:
+          <input
+            type="password"
+            value={userForUpdate?.password ?? ''}
+            onChange={(event) => setUserForUpdate({ ...userForUpdate, ...{ password: event.target.value } })}
+            required
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default UpdateUser;
--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import { deleteUser } from '../../services/users';
-import CreateUser from './components/createUser';
+import { CreateUser, UpdateUser } from './components/createUser';
 
 import styles from './users.module.scss';
 
 const Users = ({ users, setUsers }) => {
-  const handleDeleteUser = (event) => {
+  const [userForUpdate, setUserForUpdate] = useState();
+
+  const qqq = (event) => {
     const {
-      target: { id },
+      currentTarget: { id },
     } = event;
-    if (event.target.tagName === 'SPAN') {
+
+    if (event.target.id.startsWith('delete__')) {
       deleteUser(id);
       const updatedUsers = users.filter((user) => user.id !== id);
       setUsers(updatedUsers);
+    }
+
+    if (event.target.id.startsWith('update__')) {
+      const qqq = users.find((user) => user.id === id);
+      setUserForUpdate(qqq);
     }
   };
 
@@ -21,16 +30,20 @@ const Users = ({ users, setUsers }) => {
         <p>Users:</p>
         <div>
           {users.map(({ id, name }, index) => (
-            <p className={styles.user} key={id} id={id} onClick={handleDeleteUser}>
+            <p className={styles.user} key={id} id={id} onClick={qqq}>
               {index + 1}) {name}{' '}
-              <span id={id} className={styles.deleteUser}>
+              <span id={`delete__${id}`} className={styles.deleteUser}>
                 Delete
+              </span>
+              <span id={`update__${id}`} className={styles.updateUser}>
+                Update
               </span>
             </p>
           ))}
         </div>
       </div>
 
+      <UpdateUser users={users} setUsers={setUsers} userForUpdate={userForUpdate} setUserForUpdate={setUserForUpdate} />
       <CreateUser users={users} setUsers={setUsers} />
     </div>
   );
